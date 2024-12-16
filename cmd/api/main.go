daf06ae6382354192f59e177201f5ada4d6e2415
wafieabiyya <wafieabiyya7@gmail.com>
2024-12-16 19:13:08 +0700

    chore: adding env variable

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
 	cfg := config{
-		addr: ":8080",
+		addr: port,
 	}
 
 	app := &application{
@@ -13,4 +26,5 @@ func main() {
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/wafieabiyya/goropher-social.git
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
